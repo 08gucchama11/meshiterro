58037631f0c9982e2982c8dc844fddaab1de6f87
08gucchama11 <296365775+08gucchama11@users.noreply.github.com>
2026-07-21 00:59:26 +0000

    [Finish] 4-14

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,10 +19,21 @@ class UsersController < ApplicationController
     @post_images = @user.post_images
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user)
+  end
+
+
   private
 
   def user_params
-    params.require(:user).permit(:name, :email_address, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email_address, :password, :password_confirmation, :profile_image)
   end
 
 end
